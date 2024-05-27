47b5858ac31d6cb78a967ccb7778f117df6dd287
Laura Ophélie <lauraophelie1@gmail.com>
2024-05-27 17:05:30 +0200

    landing-page

--- src/pages/home/ArrowButton.tsx
+++ src/pages/home/ArrowButton.tsx
@@ -1,9 +1,14 @@
+import { IonIcon } from "@ionic/react";
+import { arrowForwardOutline } from 'ionicons/icons';
 import "./ArrowButton.scss";
 
 const ArrowButton: React.FC = () => {
     return (
         <div className="arrow-button">
-            
+            <IonIcon 
+                icon={arrowForwardOutline}
+                className="arrow-button__icon"
+            />
         </div>
     )
 }
