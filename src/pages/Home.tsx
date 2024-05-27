1579457cb0215384d7ade29e1398ce3336d0d768
Laura Ophélie <lauraophelie1@gmail.com>
2024-05-27 16:23:03 +0200

    landing-page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,25 @@
 import { IonPage } from '@ionic/react';
-import './Home.css';
+import './Home.scss';
+import ArrowButton from './home/ArrowButton';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      
+    <IonPage className="landing">
+      <div className="landing__display">
+
+      </div>
+      <div className="landing__getting-started">
+        <h1 className="landing__getting-started--title">
+          <p>
+            Fresh Starts
+          </p>
+          <p>
+            Every Morning
+          </p>
+        </h1>
+
+        <ArrowButton />
+      </div>
     </IonPage>
   );
 };
---
+++ src/pages/home/ArrowButton.tsx
@@ -0,0 +1,11 @@
+import "./ArrowButton.scss";
+
+const ArrowButton: React.FC = () => {
+    return (
+        <div className="arrow-button">
+            
+        </div>
+    )
+}
+
+export default ArrowButton;
