48136ac5067b5a0d8a9624f0e6c62dd44d240c4e
Laura Ophélie <lauraophelie1@gmail.com>
2024-06-02 15:37:55 +0200

    landing-page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,17 @@ const Home: React.FC = () => {
   return (
     <IonPage className="landing">
       <div className="landing__display">
-
+          <div className="landing__display__box landing__display__box--one"> </div>
+          <div className="landing__display__box landing__display__box--two"> 
+              <h3 className="landing__display__box landing__display__box--two__title">
+                Breezy
+              </h3>
+          </div>
+          <div className="landing__display__box landing__display__box--three"> </div>
+          <div className="landing__display__box landing__display__box--four"> </div>
+          <div className="landing__display__box landing__display__box--five"> </div>
+          <div className="landing__display__box landing__display__box--six"> </div>
+          <div className="landing__display__box landing__display__box--seven"> </div>
       </div>
       <div className="landing__getting-started">
         <h1 className="landing__getting-started--title">
