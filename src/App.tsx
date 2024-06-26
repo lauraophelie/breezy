f881c05702acb37e559e89c4978ebaa9986f6f86
Laura Ophélie <lauraophelie1@gmail.com>
2024-06-26 21:35:47 +0200

    update components, login page, mixin

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from './pages/login/Login';
 
 setupIonicReact();
 
@@ -45,6 +46,11 @@ const App: React.FC = () => (
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
+        <Route
+          path="/login"
+        >
+          <Login />
+        </Route> 
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,21 @@
+import { IonPage } from "@ionic/react";
+import "./Login.scss";
+
 const Login : React.FC = () => {
     return (
-        <>
-        </>
+        <IonPage className="login">
+            <div className="login__display">
+                <div className="login__display__box login__display__box--one">
+
+                </div>
+                <div className="login__display__box login__display__box--two">
+
+                </div>
+            </div>
+            <div className="login__content">
+
+            </div>
+        </IonPage>
     )
 }
 
