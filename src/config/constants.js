4743ddb852686fe6dd6ddc6a76fe31181803d377
Laura Ophélie <lauraophelie1@gmail.com>
2024-06-27 00:17:51 +0200

    login page done

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const IMG_BUBBLE_DATA_FOLDER = "src/imgs/";
+export const IMG_BUBBLE_DATA_FOLDER = "src/assets/imgs/";
 export const APP_NAME = "Breezy";
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,17 +3,34 @@ import "./Login.scss";
 import InputComponent from "../../components/input/InputComponent";
 import Bouton from "../../components/button/Bouton";
 
+import { IMG_BUBBLE_DATA_FOLDER } from "../../config/constants";
+import ImageBubble from "../home/components/ImageBubble";
+
 const Login : React.FC = () => {
+
+    const bubbleData = [
+        {
+            image: IMG_BUBBLE_DATA_FOLDER + "im7.jpg",
+            className: "login__display__box login__display__box--one"
+        },
+        {
+            image: IMG_BUBBLE_DATA_FOLDER + "im8.jpg",
+            className: "login__display__box login__display__box--two"
+        }
+    ];
+
     return (
         <IonPage className="login">
             <div className="login__display">
-                <div className="login__display__box login__display__box--one">
-                
-                </div>
-                <div className="login__display__box login__display__box--two">
-                    
-                </div>
+                {bubbleData.map((item, index) => (
+                    <ImageBubble
+                        key={index}
+                        className={item.className}
+                        imageSrc={item.image}
+                    />
+                ))}
             </div>
+            
             <div className="login__content">
                 <h1 className="login__content__title">
                     Log In
